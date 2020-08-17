0ef35b4f4fcc99480659d460b482cf0d79c3a8bd
balajich <balaji.chopparapu@gmail.com>
2020-08-17 10:25:26 +0530

    Fixed Typo

--- restapi/src/main/java/org/eduami/spring/restapi/HelloWorldController.java
+++ restapi/src/main/java/org/eduami/spring/restapi/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorlController {
+public class HelloWorldController {
     @Value("${server.port}")
     private String serverPort;
 
